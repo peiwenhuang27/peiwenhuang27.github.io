69bb95d435c798f88714c6099a292a01183a089c
peiwenhuang27 <b07705025@ntu.edu.tw>
2022-11-14 00:57:08 +0800

    petit-mood draft done

--- src/Components/Category.js
+++ src/Components/Category.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route, Link, useParams, useMatch, Outlet } from "react-router-dom";
+import { Link, useParams, Outlet } from "react-router-dom";
 import { LeftLinkNav } from "./Pages/Home/LeftNav";
 import TopicCard from "./Pages/Home/TopicCard";
 
@@ -12,7 +12,7 @@ function MediumList (meta) {
                 {
                     data.items.map(item =>
                         <li className="col-md-6 mb-3">
-                            <TopicCard link={item.title.replace(/\s/g , "-")} data={item} />
+                            <TopicCard link={item.id} data={item} />
                         </li>
                     )
                 }
@@ -30,7 +30,7 @@ function SmallList (meta) {
                 {
                     data.items.map(item =>
                         <li className="col-md-4 mb-3">
-                            <TopicCard link={item.title.replace(/\s/g , "-")} data={item} />
+                            <TopicCard link={item.id} data={item} />
                         </li>
                     )
                 }
@@ -53,12 +53,12 @@ function LargeList (meta) {
             </div>
             
             <div className="col-md-9">
-                <div className="large-list w-100 d-flex flex-column align-items-center mb-3">
-                    <ul className="d-flex flex-column align-items-center">
+                <div className="large-list d-flex flex-column align-items-center mb-3">
+                    <ul className="w-100 d-flex flex-column align-items-center">
                         {
                             category.items.map(item =>
-                                <li className="m-0">
-                                    <TopicCard link={item.title.replace(/\s/g , "-")} data={item} />
+                                <li className="w-100 m-0">
+                                    <TopicCard link={item.id} data={item} />
                                 </li>
                             )
                         }
@@ -73,7 +73,7 @@ function LargeList (meta) {
 function Category (meta) {
     let { categoryId } = useParams();
     const data = meta.data;
-    let categoryIndex = data.findIndex(data => data.suffix == categoryId);
+    let categoryIndex = data.findIndex(data => data.suffix === categoryId);
 
 
     return (
