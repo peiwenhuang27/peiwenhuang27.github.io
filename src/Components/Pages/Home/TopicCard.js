3c968f10fd5f943d7215de8a61e4fe9b3ee64a7e
peiwenhuang27 <b07705025@ntu.edu.tw>
2022-11-14 00:57:08 +0800

    petit-mood draft done

--- src/Components/Pages/Home/TopicCard.js
+++ src/Components/Pages/Home/TopicCard.js
@@ -1,24 +1,23 @@
 import { Link, Outlet } from "react-router-dom";
 
 function TopicCard (meta) {
-
-    // const prefix = meta.prefix; // '...category/'
     const link = meta.link;
     const data = meta.data; // title, tags, desc, img
-    // let workId = data.title.replace(/\s/g , "-");
-    let img_src = process.env.PUBLIC_URL + '/cover.jpg';
-    // TODO: change img_src `${prefix}/${workId}`
     
     return (
-        <div>
-            <Link to={link} className="topic-card text-decoration-none mb-5">
-                <div className="card mx-3">
-                    <img className="card-img-top" src={img_src} alt="Card image cap" />
+        <div className="mb-5 px-3">
+            <Link to={link} className="topic-card text-decoration-none">
+                <div className="topic-card-img" style={{ backgroundImage:`url(${data.img})` }}></div>
+                {/* <div className="card px-3">
+                    <img className="card-img" src={data.img} alt="Card image cap" />
+                </div> */}
+                <div className="topic-text-container my-4">
+                    <div className="flex-column align-items-start">
+                        <h3 className="pb-3">{data.title}</h3>
+                        <p>{data.desc}</p>
+                    </div>
                 </div>
-                <div className="flex-column align-items-start px-4 mt-4 mb-5">
-                    <h3 className="pl-3 pb-3">{data.title}</h3>
-                    <p>{data.desc}</p>
-                </div>
+                
             </Link>
 
             <Outlet />
